e45a6790a276d476464a6d7ab6b35904dcfaf088
JhonIsNotReal <110693901+Jhonisnotreal@users.noreply.github.com>
2022-11-11 16:54:56 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-console.log("Hola Sistema Solar");
-
-//Mensaje de despedida
-console.log("Adios")
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo")
